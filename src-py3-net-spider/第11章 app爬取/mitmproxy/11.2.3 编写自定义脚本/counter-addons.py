c0b055089ebb0e649fb43be30c23877cecc6f639
ninecents <kinghzking@gmail.com>
2025-03-04 19:15:25 +0800

    【py3-net-spder】11.2.3 编写自定义脚本

---
+++ src-py3-net-spider/第11章 app爬取/mitmproxy/11.2.3 编写自定义脚本/counter-addons.py
@@ -0,0 +1,23 @@
+﻿"""
+mitmweb -s counter-addons.py
+J:\_ALL\CODE\venv\py399_spider\Scripts\mitmproxy.exe -s counter-addons.py
+
+"C:\Program Files\Google\Chrome\Application\chrome.exe"   --proxy-server=http://localhost:8080   --ignore-certificate-errors
+"""
+
+import mitmproxy.http
+from mitmproxy import ctx
+
+
+class Counter:
+    def __init__(self):
+        self.num = 0
+
+    def request(self, flow: mitmproxy.http.HTTPFlow):
+        self.num = self.num + 1
+        print("We've seen %d flows" % self.num)
+        ctx.log.info("We've seen %d flows" % self.num)
+
+addons = [
+    Counter(),
+]
---
+++ src-py3-net-spider/第11章 app爬取/mitmproxy/11.2.3 编写自定义脚本/counter-def.py
@@ -0,0 +1,15 @@
+﻿"""
+mitmweb -s counter-def.py
+J:\_ALL\CODE\venv\py399_spider\Scripts\mitmproxy.exe -s counter-def.py
+
+"C:\Program Files\Google\Chrome\Application\chrome.exe"   --proxy-server=http://localhost:8080   --ignore-certificate-errors
+"""
+
+
+import mitmproxy.http
+from mitmproxy import ctx
+num = 0
+def request(flow: mitmproxy.http.HTTPFlow):
+    global num
+    num = num + 1
+    ctx.log.info("We've seen %d flows" % num)
