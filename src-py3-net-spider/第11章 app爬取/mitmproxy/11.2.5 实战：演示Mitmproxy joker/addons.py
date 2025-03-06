5c0605658d705d92eb395cf698aec892270df643
ninecents <kinghzking@gmail.com>
2025-03-06 09:38:33 +0800

    【py3-net-spder】11.2.5 实战：演示Mitmproxy joker

---
+++ src-py3-net-spider/第11章 app爬取/mitmproxy/11.2.5 实战：演示Mitmproxy joker/addons.py
@@ -0,0 +1,19 @@
+﻿"""
+### http.HTTPResponse.make(404) 在python3.8.8版本中是正常的，在python3.9.9中不支持，待查
+
+cd /d J:\_ALL\CODE\github\ninecents\python3-web-spider-learning\src-py3-net-spider\第11章 app爬取\mitmproxy\11.2.5 实战：演示Mitmproxy joker
+J:\_ALL\CODE\venv\py388_spider\Scripts\mitmdump.exe -s addons.py
+J:\_ALL\CODE\venv\py399_spider\Scripts\mitmdump.exe -s addons.py
+
+"C:\Program Files\Google\Chrome\Application\chrome.exe"   --proxy-server=http://localhost:8080   --ignore-certificate-errors
+"""
+
+
+import counter
+import joker
+
+addons = [
+    counter.Counter(),
+    joker.Joker(),
+]
+
---
+++ src-py3-net-spider/第11章 app爬取/mitmproxy/11.2.5 实战：演示Mitmproxy joker/counter.py
@@ -0,0 +1,11 @@
+﻿import mitmproxy.http
+from mitmproxy import ctx
+class Counter:
+    def __init__(self):
+        self.num = 0
+
+    def request(self, flow: mitmproxy.http.HTTPFlow):
+        self.num = self.num + 1
+        # 使用 -w 参数指定输出文件
+        # mitmproxy -w output.log
+        ctx.log.error("We've seen %d flows" % self.num)
